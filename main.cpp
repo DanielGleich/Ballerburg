9a5cff95c8932afaf358221c177495282d66a077
DanielGleich <gleichdaniel16@gmail.com>
2020-06-09 19:31:01 +0200

    Tests entfernt, Font eingebunden

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
-#include "widget.h"
 #include <QApplication>
+#include <QWidget>
+#include <QPushButton>
+#include <QFont>
 
 int main(int argc, char *argv[])
 {
   QApplication a(argc, argv);
-  Widget w;
-  w.show();
+  QWidget w;
+  w.setFixedSize(300,200);
+  w.setWindowTitle("Exit Button Übung");
 
+  QPushButton pbExit("Beenden",&w);
+  pbExit.setGeometry(55,70,200,40);
+
+  QFont myFont;
+  myFont.setBold(true);
+  myFont.setFamily("Times");
+  myFont.setPixelSize(18);
+  pbExit.setFont(myFont);
+
+  QObject::connect(&pbExit,SIGNAL(clicked(bool)),&w,SLOT( close() )  );
+
+  w.show();
   return a.exec();
 }
